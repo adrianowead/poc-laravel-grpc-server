993e26cb886a25ba7d866a522b9d74f9abb5be62
Adriano Maciel <adriano_mail@hotmail.com>
2022-03-23 15:55:29 -0300

    Adicionada tabela de score no MySQL

--- docker/mysql/startup.sql
+++ docker/mysql/startup.sql
@@ -5,3 +5,11 @@ CREATE TABLE IF NOT EXISTS `meu_db`.`tb_users` (
   `password` VARCHAR(255) NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
+
+CREATE TABLE IF NOT EXISTS `meu_db`.`tb_score` (
+  `id` BIGINT NOT NULL AUTO_INCREMENT,
+  `user_id` BIGINT NOT NULL,
+  `score` DECIMAL(10,2) NULL,
+  PRIMARY KEY (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `meu_db`.`tb_users` (`id`),
+  UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
